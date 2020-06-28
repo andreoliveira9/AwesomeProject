1029962c1c6cc2ac77e71585e0d060d510eb46f5
André Oliveira <andreprogramador10@gmail.com>
2020-06-28 13:45:59 -0300

    projeto navigator

---
+++ src/routes.js
@@ -0,0 +1,8 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Main from './pages/main';
+
+export default createStackNavigator({
+    Main 
+});
